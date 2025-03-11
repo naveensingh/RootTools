d3e07baa8e4b0fd487500065b6bf500b16229f2e
Naveen Singh <snaveen935@gmail.com>
2025-03-12 03:50:48 +0530

    Add maven publication configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,8 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.library") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+}
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.6.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
-    }
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
 }
--- roottools/build.gradle.kts
+++ roottools/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.library")
+    id("maven-publish")
 }
+
+group = "com.stericson"
+version = "1.0.0"
 
 android {
     namespace = "com.stericson.RootTools"
@@ -12,13 +16,39 @@ android {
         version = project.version.toString()
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            consumerProguardFiles("proguard-rules.pro")
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
+
+    publishing {
+        singleVariant("release")
     }
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.github.Stericson:RootShell:1.6")
+}
+
+publishing.publications {
+    create<MavenPublication>("release") {
+        afterEvaluate {
+            from(components["release"])
+        }
+    }
 }
